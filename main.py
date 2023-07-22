e756f99a50e0de74935089d01a2652e40209018a
caner <caner@elephantapps.co>
2023-07-22 19:51:26 +0300

    replaced async instead of sync in sqlalchemy

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
+from src.db.engine import engine
+from src.definitions import Base
 
 app = FastAPI()
-
 
 
 @app.get("/healthcheck")
 async def health():
     return {"status": "I feel... not too bad. not too good too. At least I am alive."}
 
+
+@app.on_event("startup")
+async def init_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
--- src/definitions.py
+++ src/definitions.py
@@ -1,4 +1,10 @@
 from pathlib import Path
-
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
 
 ROOT_DIR = Path(__file__).parent.parent.__str__()
+
+
+metadata = sqlalchemy.MetaData()
+
+Base = declarative_base(metadata=metadata)
